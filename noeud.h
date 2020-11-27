742c6c8a56e0f021216422fe931486471d6dd9b0
hadjerbek <hadjerbek22@gmail.com>
2020-11-27 19:29:06 +0100

    creation d'ABR

---
+++ noeud.h
@@ -0,0 +1,17 @@
+//
+//  noeud.h
+//  ProjetDictionnaire
+//
+//  Created by Hadjer Bekhira on 27/11/2020.
+//
+
+#ifndef noeud_h
+#define noeud_h
+
+struct noeud{
+    char* mot;
+    int nb_occurence;
+}
+
+
+#endif /* noeud_h */
